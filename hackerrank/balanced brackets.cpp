8e42c039cbd761170a7da02c0538d1cd86e29287
githubprabh <prabhnoor1998@gmail.com>
2018-05-17 14:36:20 +0530

    Create balanced brackets.cpp

---
+++ hackerrank/balanced brackets.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>   // worrking
+#include <string>
+
+using namespace std;
+
+// checks whether or not two characters are opening and closing
+bool Brackets(char opening, char closing)
+{
+	if( opening == '(' && closing == ')' ) return true;
+	else if ( opening == '[' && closing == ']' ) return true;
+	else if ( opening == '{' && closing == '}' ) return true;
+	return false;
+}
+
+// checks for when
+bool BalancedParentheses(string b)
+{
+	stack<char> a;
+	for(int i = 0; i < b.length(); i++)
+	{
+		if( b[i] == '(' || b[i] == '{' || b[i] == '[' )
+			a.push(b[i]);
+		else if( b[i] == ')' || b[i] == '}' || b[i] == ']' )
+		{
+			if( a.empty() || !Brackets( a.top(), b[i] ) )
+				return false;
+			else
+				a.pop();
+		}
+	}
+	return a.empty();
+}
+
+int main()
+{
+	// tests BalancedParentheses function
+	int t;
+	cin>>t;
+	while(t--){
+	string c;
+	cin >> c;
+	if(BalancedParentheses(c))
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+}
+}
